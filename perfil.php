84690423ddc7b9ca21ae5640c6bead42633227c6
Vicenç Juan Tomàs Montserrat <vtomasr5@gmail.com>
2012-12-27 15:48:20 +0100

    añadido para que el usuario no pueda hacer zoom

--- perfil.php
+++ perfil.php
@@ -5,7 +5,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
     
     <!-- [portable options] -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0;" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0; user-scalable=0;" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
     
     <!-- [SEO] -->
